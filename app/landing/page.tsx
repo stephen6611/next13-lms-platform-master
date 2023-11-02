3ab198b0bb13f7fff519c28618d876223f502740
stephenandres <stephenandreas04@gmail.com>
2023-11-02 13:22:23 +0700

    11/2

---
+++ app/landing/page.tsx
@@ -0,0 +1,15 @@
+import { SearchInput } from "@/components/search-input";
+
+
+
+const tsrsrt = () => {
+  return ( 
+    <>
+    <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        <SearchInput />
+      </div>
+    </>
+   );
+}
+ 
+export default tsrsrt;
